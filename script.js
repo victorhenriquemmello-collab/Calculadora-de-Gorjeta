2978c594f2f55007503b19a3e54d39a8e82a9f45
victorhenriquemmello-collab <victorhenriquemmello@gmail.com>
2026-04-21 15:36:32 -0300

    Refactor input handling for 'pessoas' value
    Removed the default value for 'pessoas' input and adjusted calculations accordingly.

--- script.js
+++ script.js
@@ -3,7 +3,6 @@ const btnCalcular = document.querySelector("#calcular");
 btnCalcular.addEventListener("click", () => {
     const conta = parseFloat(document.querySelector("#totalConta").value);
     const taxa = parseFloat(document.querySelector("#porcentagemGorjeta").value);
-    const pessoas = parseInt(document.querySelector ("#pessoas").value) || 1;
 
     if (isNaN(conta) || conta <= 0) {
         alert("Por favor, insira um valor válido para a conta.");
@@ -12,7 +11,7 @@ btnCalcular.addEventListener("click", () => {
 
     const gorjetaTotal = conta * taxa;
     const contaTotal = conta + gorjetaTotal;
-    const valorPorPessoa = contaTotal / pessoas;
+
 
     document.querySelector("#valorGorjeta").innerText = gorjetaTotal.toLocaleString('pt-br', {style: 'currency', currency: 'BRL'});
 
